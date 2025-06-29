f4a9e61a0e3c0c723af4fe25183d1da6ff4b9225
joeeey <513358386@qq.com>
2025-06-29 12:25:43 +0800

    Fix hpa

--- assignment2-k8s/k8s/deploy-namespaced.sh
+++ assignment2-k8s/k8s/deploy-namespaced.sh
@@ -38,7 +38,8 @@ echo "⏳ Giving webhook service additional time to stabilize..."
 sleep 15
 
 echo "📊 Deploying Metrics-server..."
-kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl apply -f metrics-server.yaml
+# kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 kubectl patch deployment metrics-server -n kube-system \
   --type json -p='[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--kubelet-insecure-tls"}]'
 
